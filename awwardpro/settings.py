75552364b01cbb3005be2f3e867374780b502930
sivernorman <silvernorman.o@gmail.com>
2022-03-17 14:23:57 +0300

    heroku deployment

--- awwardpro/settings.py
+++ awwardpro/settings.py
@@ -29,8 +29,7 @@ MODE=config("MODE", default="dev")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-
+DEBUG = os.environ.get('DEBUG', False)
 ALLOWED_HOSTS = []
 
 
